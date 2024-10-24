52733e94f72b7c3770356bc5cf6816592d950679
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-10-23 21:39:41 -0700

    wip

--- module.php
+++ module.php
@@ -17,7 +17,7 @@ return [
         ->register('/cookies', 'cookies', 'cookie/cookie_notice', \NamelessMC\CookieConsent\Pages\Cookies::class, false),
     (new Extend\PanelPages)
         ->templateDirectory(__DIR__ . '/panel_views')
-        ->register('/panel/cookies', 'cookie_settings', 'cookie/cookies', \NamelessMC\CookieConsent\Pages\Panel\Cookies::class, 'admincp.cookies', 'fas fa-cookie-bite'),
+        ->register('/cookies', 'cookie_settings', 'cookie/cookies', \NamelessMC\CookieConsent\Pages\Panel\Cookies::class, 'admincp.cookies', 'fas fa-cookie-bite'),
     (new Extend\Permissions)
         ->register([
             'staffcp' => [
