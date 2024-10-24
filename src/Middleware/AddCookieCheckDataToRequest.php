757b1a65c667b9e882bf74e9862cd6fd6dc17b53
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-10-24 11:38:10 -0700

    wip

--- src/Middleware/AddCookieCheckDataToRequest.php
+++ src/Middleware/AddCookieCheckDataToRequest.php
@@ -120,8 +120,6 @@ class AddCookieCheckDataToRequest extends \NamelessMC\Framework\Pages\Middleware
 
         $json = json_encode($script_options, JSON_PRETTY_PRINT);
 
-        $file = file_get_contents(__DIR__ . '/../../assets/frontend/js/template.js');
-        dd($file);
         return str_replace(
             '//"{x}"',
             substr($json, 1, -1),
