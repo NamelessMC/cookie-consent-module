293330aa3809cec34f69d1b20b067cd76a532176
Tadhg Boyle <tadhgsmboyle@gmail.com>
2024-10-23 21:49:12 -0700

    wip

--- module.php
+++ module.php
@@ -13,10 +13,8 @@ return [
     (new Extend\Sitemap) // TODO could this be done via FrontendPages ?
         ->path('/cookies', 0.9),
     (new Extend\FrontendPages)
-        ->templateDirectory(__DIR__ . '/views')
         ->register('/cookies', 'cookies', 'cookie/cookie_notice', \NamelessMC\CookieConsent\Pages\Cookies::class, false),
     (new Extend\PanelPages)
-        ->templateDirectory(__DIR__ . '/panel_views')
         ->register('/cookies', 'cookie_settings', 'cookie/cookies', \NamelessMC\CookieConsent\Pages\Panel\Cookies::class, 'admincp.cookies', 'fas fa-cookie-bite'),
     (new Extend\Permissions)
         ->register([
